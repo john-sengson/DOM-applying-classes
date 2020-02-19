b1d0ab40c29f6045430b59270935917e55b880ac
john-sengson <j10i_freedom@hotmail.com>
2020-02-20 03:23:13 +1300

    failed attempt

--- classes.js
+++ classes.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  // one();
+   //two();
+   three();
   // Your turn! Create a new function called `two`, then call it from here.
+  
 }
-
 function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
@@ -20,6 +21,14 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two () {
+  var two = document.getElementById('two')
+  one.classList.add('green')
+}
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  one.classList.add('red')
+}
 
 // CREATE FUNCTION makeVisible HERE
